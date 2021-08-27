8fb0e1e44490b52fe44773a658502a5bc9405c0c
Santhosh Chinnasamy <csesanthosh15@gmail.com>
2021-08-27 19:56:43 +0530

    Santhosh: add data and design

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap",
+    "gatsby-plugin-postcss",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,121 @@
-import * as React from "react"
+import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faMedium,
+  faGoogle,
+  faInstagram,
+  faTwitter,
+  faChrome,
+} from "@fortawesome/free-brands-svg-icons";
+import { faShareSquare } from "@fortawesome/free-regular-svg-icons";
+import "../styles/global.scss";
+import * as info from "../data/info";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const calculateExperience = (
+  startDate = new Date(),
+  dateToCalculate = new Date()
+) => {
+  const startTime = new Date(startDate).getTime();
+  const dateToCompare = new Date(dateToCalculate).getTime();
+  const totalYears = (dateToCompare - startTime) / (365 * 24 * 60 * 60 * 1000);
+  const postfix = parseInt(totalYears) > 1 ? "Years" : "Year";
+  return `${Math.abs(totalYears).toFixed(1)} ${postfix} of experience`;
+};
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
 const IndexPage = () => {
+  const handleOnClick = () => {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: `${info.name} | ${info.designation}`,
+          text: `Check out ${info.name} profile`,
+          url: document.location.href,
+        })
+        .then(() => {
+          console.log("Successfully shared");
+        })
+        .catch((error) => {
+          console.error("Something went wrong sharing the blog", error);
+        });
+    }
+  };
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
+    <>
+      <div class="container mx-auto my-10 sm:px-20  flex justify-center">
+        <div class=" rounded-xl overflow-hidden border w-full lg:w-4/12 md:w-4/12 bg-white mx-3 md:mx-0 lg:mx-0 shadow-2xl">
+          <div class="w-full flex justify-between p-3">
+            <div class="flex">
+              <div class="rounded-full h-10 w-10 bg-gray-500 flex items-center justify-center overflow-hidden">
+                <img src={info.photo} alt="profilepic" />
+              </div>
+              <span class="pt-1 ml-2 font-bold text-purple-700 text-sm">
+                {info.name}
+                <p class="font-normal text-gray-500 text-sm">
+                  {info.designation}, {calculateExperience(info.startdate)}
+                </p>
+              </span>
+            </div>
+          </div>
+          <img class="h-100 w-full" src={info.photo} alt={info.name} />
+          <div class="px-3 pb-2">
+            <div class="pt-2">
               <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+                href={`mailto:${info.social.mail}`}
+                target="_blank"
+                rel="noreferrer"
               >
-                {link.text}
+                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                  <FontAwesomeIcon icon={faGoogle} />
+                </i>
               </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+              <a href={info.social.linkedin} target="_blank" rel="noreferrer">
+                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </i>
+              </a>
+              <a href={info.social.github} target="_blank" rel="noreferrer">
+                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                  <FontAwesomeIcon icon={faGithub} />
+                </i>
+              </a>
+              <a href={info.social.twitter} target="_blank" rel="noreferrer">
+                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                  <FontAwesomeIcon icon={faChrome} />
+                </i>
+              </a>
+              <a href={info.social.blog} target="_blank" rel="noreferrer">
+                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </i>
+              </a>
+              <a href={info.social.instagram} target="_blank" rel="noreferrer">
+                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </i>
+              </a>
 
-export default IndexPage
+              <button class="float-right" onClick={handleOnClick}>
+                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                  <FontAwesomeIcon icon={faShareSquare} />
+                </i>
+              </button>
+            </div>
+            <div class="pt-1">
+              <div class="mb-2 text-sm">
+                {/* <span class="font-medium mr-2">{info.name}</span> */}
+                {info.stack.map((t, i) => (
+                  <span class="text-sm text-blue-700 font-medium" key={i}>
+                    #{t}{" "}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default IndexPage;
