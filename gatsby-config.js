351a99c2f227379140a402f8443e923ff3142422
Santhosh Chinnasamy <csesanthosh15@gmail.com>
2021-08-28 09:36:02 +0530

    update gtag config

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,13 +36,13 @@ module.exports = {
         // This config will be shared across all trackingIds
         gtagConfig: {
           // optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: false,
+          anonymize_ip: true,
           cookie_expires: 0,
         },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: true,
+          head: false,
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
