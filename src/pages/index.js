452dcfacb7409ba41fbf28d5140730e327386188
Santhosh Chinnasamy <csesanthosh15@gmail.com>
2021-08-28 09:26:24 +0530

    add gtag config

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,70 +52,70 @@ const IndexPage = () => {
         </title>
         <link rel="canonical" href={info.website} />
       </Helmet>
-      <div class="container mx-auto my-10 sm:px-20  flex justify-center">
-        <div class=" rounded-xl overflow-hidden border w-full lg:w-4/12 md:w-4/12 bg-white mx-3 md:mx-0 lg:mx-0 shadow-2xl">
-          <div class="w-full flex justify-between p-3">
-            <div class="flex">
-              <div class="rounded-full h-10 w-10 bg-gray-500 flex items-center justify-center overflow-hidden">
+      <div className="container mx-auto my-10 sm:px-20  flex justify-center">
+        <div className=" rounded-xl overflow-hidden border w-full lg:w-4/12 md:w-4/12 bg-white mx-3 md:mx-0 lg:mx-0 shadow-2xl">
+          <div className="w-full flex justify-between p-3">
+            <div className="flex">
+              <div className="rounded-full h-10 w-10 bg-gray-500 flex items-center justify-center overflow-hidden">
                 <img src={info.photo} alt="profilepic" />
               </div>
-              <span class="pt-1 ml-2 font-bold text-purple-700 text-sm">
+              <span className="pt-1 ml-2 font-bold text-purple-700 text-sm">
                 {info.name}
-                <p class="font-normal text-gray-500 text-sm">
+                <p className="font-normal text-gray-500 text-sm">
                   {info.designation}, {calculateExperience(info.startdate)}
                 </p>
               </span>
             </div>
           </div>
-          <img class="h-100 w-full" src={info.photo} alt={info.name} />
-          <div class="px-3 pb-2">
-            <div class="pt-2">
+          <img className="h-100 w-full" src={info.photo} alt={info.name} />
+          <div className="px-3 pb-2">
+            <div className="pt-2">
               <a
                 href={`mailto:${info.social.mail}`}
                 target="_blank"
                 rel="noreferrer"
               >
-                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                <i className="p-2 cursor-pointer text-xl text-purple-700">
                   <FontAwesomeIcon icon={faGoogle} />
                 </i>
               </a>
               <a href={info.social.linkedin} target="_blank" rel="noreferrer">
-                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                <i className="p-2 cursor-pointer text-xl text-purple-700">
                   <FontAwesomeIcon icon={faLinkedin} />
                 </i>
               </a>
               <a href={info.social.github} target="_blank" rel="noreferrer">
-                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                <i className="p-2 cursor-pointer text-xl text-purple-700">
                   <FontAwesomeIcon icon={faGithub} />
                 </i>
               </a>
               <a href={info.social.twitter} target="_blank" rel="noreferrer">
-                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                <i className="p-2 cursor-pointer text-xl text-purple-700">
                   <FontAwesomeIcon icon={faChrome} />
                 </i>
               </a>
               <a href={info.social.blog} target="_blank" rel="noreferrer">
-                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                <i className="p-2 cursor-pointer text-xl text-purple-700">
                   <FontAwesomeIcon icon={faTwitter} />
                 </i>
               </a>
               <a href={info.social.instagram} target="_blank" rel="noreferrer">
-                <i class="p-2 cursor-pointer text-xl text-purple-700">
+                <i className="p-2 cursor-pointer text-xl text-purple-700">
                   <FontAwesomeIcon icon={faInstagram} />
                 </i>
               </a>
 
-              <button class="float-right" onClick={handleOnClick}>
-                <i class="p-2 cursor-pointer text-xl text-purple-700">
+              <button className="float-right" onClick={handleOnClick}>
+                <i className="p-2 cursor-pointer text-xl text-purple-700">
                   <FontAwesomeIcon icon={faShareSquare} />
                 </i>
               </button>
             </div>
-            <div class="pt-1">
-              <div class="mb-2 text-sm">
-                {/* <span class="font-medium mr-2">{info.name}</span> */}
+            <div className="pt-1">
+              <div className="mb-2 text-sm">
+                {/* <span className="font-medium mr-2">{info.name}</span> */}
                 {info.stack.map((t, i) => (
-                  <span class="text-sm text-blue-700 font-medium" key={i}>
+                  <span className="text-sm text-blue-700 font-medium" key={i}>
                     #{t}{" "}
                   </span>
                 ))}
